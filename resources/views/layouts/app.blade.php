ea3b0af1dafed51617a77bf47b14a9d820e18b4f
MoralesWill99 <moraleswil93@mail.com>
2025-11-15 14:30:58 -0600

    cambios en las vistas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,8 +26,8 @@
                         <div class="flex">
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
-                                <a href="{{ url('/') }}">
-                                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                                <a href="{{ url('/dashboard') }}">
+                                <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                                 </a>
                             </div>
 
